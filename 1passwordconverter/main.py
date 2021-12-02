e1964373298eb46f9d69a4326a35576923662ca9
Shayne Hartford <shaybox@shaybox.com>
2021-12-02 02:35:11 -0500

    Upload program

---
+++ 1passwordconverter/main.py
@@ -0,0 +1,39 @@
+import click
+import csv
+import json
+
+
+@click.command()
+@click.option(
+    "file",
+    "--file",
+    "-f",
+    default="export.data",
+    help="JSON file to convert",
+    prompt="Which file?",
+    type=click.File("r"),
+)
+def main(file):
+    data = json.load(file)
+    for account in data["accounts"]:
+        print(f"Processing account: {account['attrs']['name']}")
+        for vault in account["vaults"]:
+            vault_name = vault["attrs"]["name"]
+            print(f"Processing vault: {vault_name}")
+            with open(f"{vault_name}.csv", "w") as csv_file:
+                writer = csv.writer(csv_file)
+                for item in vault["items"]:
+                    item = item["item"]
+                    overview = item["overview"]
+                    title = overview["title"]
+                    url = overview["url"]
+                    print(f"Processing item: {title}")
+                    username, password = None, None
+                    for field in item["details"]["loginFields"]:
+                        if not hasattr(field, "designation"):
+                            continue
+                        if field["designation"] == "username":
+                            username = field["value"]
+                        if field["designation"] == "password":
+                            password = field["value"]
+                    writer.writerow([title, url, username, password])
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 1PasswordConverter
 Convert .1pux to .csv
+
+1Password uses this new export format `.1pux`, I assume stands for `1 Password User eXport`.  
+As of right now, 1Password doesn't support importing their new export format...  
+This process is lossy, it only converts `Title/Name`, `URL`, `Username`, and `Password`.  
+This program only extracts this data because 1Password also doesn't support importing anything but those columns,  
+So if you have other data such as TOTP, files, documents, etc, you will need to manually recreate it.  
+
+### Installation
+- Install `Poetry` (via pip or package manager)
+- Build the package: `poetry build`
+- Install the package: `pip install dist/*.tar.gz`
+- Run the package: `convert`
+
+### Development
+- Install `Poetry` (via pip or package manager)
+- Install dependencies: `poetry install`
+- Run the package in venv: `poetry run convert`
